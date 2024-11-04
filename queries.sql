978a7df947dc32bc8429bdb2ba6c72d63794b634
GrizzlyMain <ryan.thom.dalton@gmail.com>
2024-11-04 12:09:31 -0600

    1st Commit - Passing Task 1

--- queries.sql
+++ queries.sql
@@ -1,4 +1,9 @@
 --Part 1
+--column = ID, Data Type = Int/Primary Key
+--column = employer, Data Type - varchar(255)
+--column = employer, Data Type - varchar(255)
+--column = employer, Data Type - varchar(255)
+-- found with SHOW FIELDS FROM job FROM techjobs; from https://stackoverflow.com/questions/1215368/how-to-get-the-mysql-table-columns-data-type
 
 --Part 2
 

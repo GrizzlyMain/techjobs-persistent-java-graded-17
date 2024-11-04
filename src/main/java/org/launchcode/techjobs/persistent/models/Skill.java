2f695e70be6d138b9756cfe628d32afdd6eabe57
GrizzlyMain <ryan.thom.dalton@gmail.com>
2024-11-04 12:54:23 -0600

    2nd Commit - Failing 6, Passing 12 on Task 2

--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,23 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
+    @NotBlank(message = "Location is required")
+    @Size(min = 3, max = 250, message = "Location must be between 3 and 250 characters")
+    private String description;
+
+    public Skill(){}
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String name) {
+        this.description = description;
+    }
 
 }
